054502b08d55ffeaae9b716e055f9e35b321d13d
tynichols <tynichols0@gmail.com>
2015-05-04 19:11:05 -0400

    Added config file and velocity to apsides function
    Added a config file which reads values for G and M, as well as giving
    the apsides() function ability to compute velocity

--- orbit_graphs.py
+++ orbit_graphs.py
@@ -2,9 +2,27 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse, Circle
 import math
 
-#the graviational constant. Note this is in km3 to facilitate the calculation of the standard gravitational parameter
-G = 0.0000000000667384
-
+def open_config(config_file):
+    """
+    opens the configuration file for values of G, M, and others
+    """
+    #open the file with the specified name
+    config = open(config_file, 'r')
+    
+    #split the file into an array of values
+    config_values = config.read().split(',')
+    
+    #declare the global constants and set them equal to the values in the config file
+    global G, M
+    
+    G = float(config_values[1])
+    M = float(config_values[3])
+    
+    #print the values of the constants
+    print G, ' is the gravitational constant in N m2 kg-2'
+    print M, ' is the mass of the planet in kg'
+    
+    
 def graph_orbit(body_radius = 10, semi_majaxis = 35, eccentricity = 0.1, arg_periapsis = 0, xbounds = (-100, 100) , ybounds = (-100, 100)):
     """
     Graphs an elliptical orbit and a spherical planetary body from semi-major axis, eccentricity, and the argument of periapsis
@@ -42,41 +60,36 @@ def graph_orbit(body_radius = 10, semi_majaxis = 35, eccentricity = 0.1, arg_per
     ax.set_xlim(xbounds[0], xbounds[1])
     ax.set_ylim(ybounds[0], ybounds[1])
 
-def compute_orbital_properties(body_mass, semi_majaxis, orbital_eccentricity):
+def compute_orbital_properties(semi_majaxis, orbital_eccentricity):
     """
     Computes many properties of an orbit based on the six orbital elements and properties of the planetary body
     """
     
     #define the orbital elements as global variables
-    global M, mu, a, e
-    M = body_mass
-    a = semi_majaxis
+    global mu, a, e
+    a = float(semi_majaxis)
     e = orbital_eccentricity
     
     #compute mu for the body's mass
-    compute_std_grav_param(M)
+    compute_std_grav_param()
     
     #compute the orbital period
     compute_orbital_period(a)
     
-    #compute the radii of the apoapsis and periapsis
+    #compute the radii of the apoapsis and periapsis and the velocities at each
     compute_apsides()
     
-def compute_std_grav_param(body_mass):
+def compute_std_grav_param():
     """
     Compute the standard gravitational parameter by using G as defined above and the mass of the body in kg
     """
     
-    #set the body's mass to a global variable
-    global M
-    M = body_mass
-    
     #define the standard gravitational parameter as a global variable for use in the other functions
     global mu
-    mu = G * body_mass
+    mu = G * M
     
     #print mu
-    print 'The standard gravitational parameter is: ', mu
+    print 'The standard gravitational parameter is: ', mu, 'm3/s2'
     
     
 def compute_orbital_period(semi_majaxis):
@@ -86,17 +99,26 @@ def compute_orbital_period(semi_majaxis):
     
     #compute the orbital formula
     global orbital_period
-    orbital_period = 2 * math.pi * math.sqrt(semi_majaxis**3 / mu)
+    orbital_period = math.sqrt(4 * (math.pi**2) * (a**3) / mu)
     
-    print orbital_period, ' seconds'
+    #print the orbital period in seconds
+    print 'The orbital period is: ', orbital_period, ' seconds'
     
 def compute_apsides():
     """
-    calculates the radii of the apsides based upon the other orbital quantities
+    calculates the radii of the apsides and the velocity at the apsides based upon the other orbital quantities
     """
-    global Ra, Rp
+    
+    #declare the radii and velocities of the apsides as global variables
+    global Ra, Rp, Va, Vp
+    
+    #calculate the radii and velocities at the apsides
     Ra = a * (1 + e)
     Rp = a * (1 - e)
+    Va = math.sqrt((2 * mu * Rp) / (Ra * (Ra + Rp)))
+    Vp = math.sqrt((2 * mu * Ra) / (Rp * (Ra + Rp)))
     
-    print 'The radius of the apoapsis is: ', Ra
-    print 'The radius of the periapsis is: ', Rp
+    print 'The radius of the apoapsis is: ', Ra, ' m'
+    print 'The velocity at apoapsis is: ', Va, 'm/s'
+    print 'The radius of the periapsis is: ', Rp, ' m'
+    print 'The velocity at periapsis is: ', Vp, ' m/s'
